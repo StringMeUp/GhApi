6545b2335b6466a48fc7bb623a3736931f309713
Samir Ramic <ramic.samirr@gmail.com>
2022-04-11 09:17:22 +0200

    - GH: Initial commit

---
+++ app/src/main/java/com/ramones/ghubapi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ramones.ghubapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
